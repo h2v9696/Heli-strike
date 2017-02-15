97a520fe06e4c169da8fa1c9b967a5b12b95899a
LAPTOP-QQPRJPUP\Do Thai Son <chinhhedspibk@gmail.com>
2017-02-15 17:32:15 +0700

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject panelNewGame;
+	public GameObject panelHowTo;
+
+	public GameObject audioOnIcon;
+	public GameObject audioOffIcon;
+
+	void Start()
+	{
+		SetSoundState ();
+	}
+
+
+
+	public void NewGame()
+	{
+		panelNewGame.SetActive (true);
+	}
+
+	public void HowToPlay()
+	{
+		panelHowTo.SetActive (true);
+	}
+
+	public void MoreGame()
+	{
+		Application.OpenURL ("http://google.com/");
+	}
+
+	public void ToggleSound()
+	{
+		if (PlayerPrefs.GetInt ("Muted", 0) == 0)
+			PlayerPrefs.SetInt ("Muted", 1);
+		else
+			PlayerPrefs.SetInt ("Muted", 0);
+		SetSoundState ();
+	}
+
+	private void SetSoundState()
+	{
+		if (PlayerPrefs.GetInt ("Muted", 0) == 0) {
+			AudioListener.volume = 1;
+			audioOnIcon.SetActive (true);
+			audioOffIcon.SetActive (false);
+		} else {
+			AudioListener.volume = 0;
+			audioOnIcon.SetActive (false);
+			audioOffIcon.SetActive (true);
+		}
+	}
+
+}
---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour {
+	public GameObject panel;
+
+
+	public void Play()
+	{}
+
+	public void MainMenu()
+	{
+		panel.SetActive (false);
+
+	}
+}
