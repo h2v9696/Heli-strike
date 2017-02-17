81d800d53f9d4ea20f5ace7edcbe465cbe2ad169
LAPTOP-QQPRJPUP\Do Thai Son <chinhhedspibk@gmail.com>
2017-02-17 11:06:16 +0700

    Destroy Area+ Story

---
+++ Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyArea : MonoBehaviour 
+{
+	void Start()
+	{Debug.Log (" ");}
+	void OnTriggerExit2D(Collider2D c)
+	{
+		Debug.Log ("Left the Area");
+		Destroy (c.gameObject);
+	}
+
+
+}
---
+++ Assets/Scripts/TextSplit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSplit : MonoBehaviour {
+	public Text textArea;
+	public string String;
+	//public string[] strings;
+	public float speed = 0.1f;
+
+	int characterIndex=0;
+	//int stringIndex=0;
+	void Start()
+	{
+		StartCoroutine (SplitTimer());
+	}
+
+	IEnumerator SplitTimer()
+	{while (1 == 1) 
+		{
+			yield return new WaitForSeconds (speed);
+			if (characterIndex > String.Length) {
+				continue;
+			}
+			textArea.text = String.Substring(0,characterIndex);
+			characterIndex++;
+			//textArea.text = strings [stringIndex].Substring[0,characterIndex];
+		}
+	}
+
+	public void SkipSplit()
+	{if (characterIndex < String.Length)
+			characterIndex = String.Length;
+	}
+}
