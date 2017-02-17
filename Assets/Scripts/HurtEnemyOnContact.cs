10eb9cfcefcbd78317a371a78dd001425685c111
quangdaopc\quangdao <khanhbklc12@gmail.com>
2017-02-17 11:04:54 +0700

    Player can destroy enemy with missile now

---
+++ Assets/Scripts/HurtEnemyOnContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemyOnContact : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Enemy") 
+		{
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+			Instantiate (explosion, other.transform.position, other.transform.rotation);
+		}
+	}
+}
