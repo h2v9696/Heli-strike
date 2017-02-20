058e750d0794f88fed755f68156909a5e6588de6
H2V <h2v9696@gmail.com>
2017-02-20 23:13:45 +0700

    Enemy Attack, Gun Controll, EnemyBullet, Hurt Player, Enemy Health Manager Added

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	public GameObject enemyBullet;
+	public PlayerController player;
+	public Transform shootPoint;
+	public float shootDelay;
+	private float shootCounter;
+	public bool canShot;
+
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+		shootCounter = 0;
+		if (!canShot) {
+			gameObject.SetActive (false);
+		}
+	}
+
+	void Update () {
+		shootCounter -= Time.deltaTime;
+		if (shootCounter<=0) {
+			StartCoroutine ("DoubleShoot");
+			shootCounter = shootDelay;
+		}
+
+	}
+	IEnumerator DoubleShoot(){
+		Instantiate (enemyBullet, shootPoint.position, gameObject.transform.rotation);
+		yield return new WaitForSeconds (0.1f);
+		Instantiate (enemyBullet, shootPoint.position, gameObject.transform.rotation);
+
+	}
+}
---
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour {
+
+	public int enemyHealth;
+	public Explosion explosion;
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (enemyHealth <= 0) {
+			Instantiate (explosion, transform.position, transform.rotation);
+
+			Destroy (gameObject);
+		}
+	}
+	public void TakeDamage(int damageTaken) {
+		enemyHealth -= damageTaken;
+
+
+	}
+}
