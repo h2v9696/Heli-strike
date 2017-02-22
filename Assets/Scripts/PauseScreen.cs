d2e07f12fa3b079aee119afc8bf335c6492614cd
Chinh <chinhhedspibk@gmail.com>
2017-02-22 15:25:21 +0700

    Speed change camera

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour {
+
+	public string titleScreen;
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+
+	void Update()
+	{
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+		} else {
+			pauseMenuCanvas.SetActive (false);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+	}
+
+	public void Resume()
+	{
+		isPaused = false;
+	}
+
+	//public void MainMenu()
+	//{
+	//	Application.LoadLevel (titleScreen);
+	//}
+}
---
+++ Assets/Scripts/SpeedNormalArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedNormalArea : MonoBehaviour {
+	public float normalSpeed = 2f;
+	void OnTriggerStay2D(Collider2D other)
+	{if (other.name == "Player")
+			ScrollBackground.speed = normalSpeed;
+
+	}
+}
