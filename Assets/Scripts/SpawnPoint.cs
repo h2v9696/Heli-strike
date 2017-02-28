02da12e6b226698f0b51ea651392578ce0636176
Chinh <chinhhedspibk@gmail.com>
2017-02-28 16:54:26 +0700

    Demo SpawnEnemy

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+	public GameObject enemy;
+	//public Transform getChild;
+	public void SpawnEnemy()
+	{	//getChild = enemy.transform.GetChild (0);
+		Instantiate (enemy, this.transform.position, enemy.transform.rotation);
+			}
+}
