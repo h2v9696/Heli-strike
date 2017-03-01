0131b54093bf66508321f315830c22bd4a1339de
H2V <h2v9696@gmail.com>
2017-03-01 17:41:21 +0700

    Replace Boss Health Manager, Add Boss Death (incomplete),  Little Change Enemy Patroll,  Added Tag Boss

---
+++ Assets/Scripts/BossHealthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealthManager : MonoBehaviour {
+
+	public int bossMaxHealth;
+	public int bossHealth;
+
+	private Animator animator;
+
+	public bool isDeath;
+
+
+	void Start () {
+		bossHealth = bossMaxHealth;
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (bossHealth <= 0) {
+
+			isDeath = true;
+
+		}
+	}
+
+	public void TakeDamage(int damageTaken) {
+		bossHealth -= damageTaken;
+		animator.SetTrigger ("Damage");
+
+	}
+}
