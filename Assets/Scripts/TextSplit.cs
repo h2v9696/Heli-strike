572516cadddf80440eb7d773c81aa0fa25942c64
Chinh <chinhhedspibk@gmail.com>
2017-03-03 09:03:51 +0700

    Lv1...

--- Assets/Scripts/TextSplit.cs
+++ Assets/Scripts/TextSplit.cs
@@ -8,8 +8,13 @@ public class TextSplit : MonoBehaviour {
 	public string String;
 	//public string[] strings;
 	public float speed = 0.1f;
-
+	public GameObject storyCanvas;
+	int finishText = 0;
 	int characterIndex=0;
+
+	public float delay = 4f;
+
+	float delayTimer;
 	//int stringIndex=0;
 	void Start()
 	{
@@ -32,5 +37,16 @@ public class TextSplit : MonoBehaviour {
 	public void SkipSplit()
 	{if (characterIndex < String.Length)
 			characterIndex = String.Length;
+		if (characterIndex == String.Length)
+			finishText = 1;
+	}
+
+	public void finishStory()
+	{if(finishText==1)
+		delayTimer += Time.deltaTime;
+
+		if (delayTimer >= delay) {
+			Destroy (storyCanvas);
+		}
 	}
 }
