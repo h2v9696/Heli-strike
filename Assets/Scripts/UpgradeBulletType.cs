670f8828f5599b192379b172393a999056e932a3
quangdaopc\quangdao <khanhbklc12@gmail.com>
2017-03-03 09:18:47 +0700

    change death move, update sound of player

--- Assets/Scripts/UpgradeBulletType.cs
+++ Assets/Scripts/UpgradeBulletType.cs
@@ -8,17 +8,39 @@ public class UpgradeBulletType : MonoBehaviour {
 	private PlayerController playerController;
 	private Animator animator;
 
+	private bool collected;
+	void Start () {
+		collected = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (collected == true && GetComponent<AudioSource>().isPlaying == false)
+		{
+			Destroy (gameObject);
+		}
+	}
+
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player") 
 		{
+	
+			GetComponent<AudioSource> ().Play ();
 			playerController = other.GetComponent<PlayerController> ();
 			animator = other.GetComponent<Animator> ();
 
 
 			playerController.changeBulletType();
 			animator.SetTrigger ("Collect");
-			Destroy (gameObject);
+			if (GetComponent<AudioSource> ().isPlaying == false) {
+				Destroy (gameObject);
+			}
+			Destroy (GetComponent<Collider2D> ());
+			Destroy (GetComponent<SpriteRenderer> ());
+
+			collected = true;
 		}
 	}
 }
