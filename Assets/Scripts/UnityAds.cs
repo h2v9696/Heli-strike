63ac0cf1cdd11d1e5553bb0a8c497f0738f3658e
h2v9696 <h2v9696@gmail.com>
2024-04-07 13:03:01 +0900

    update to Unity 2022

--- Assets/Scripts/UnityAds.cs
+++ Assets/Scripts/UnityAds.cs
@@ -8,44 +8,44 @@ using UnityEngine.Advertisements;
 
 public class UnityAds : MonoBehaviour {
 
-	public string zoneId;
-	Button m_Button;
-	void Start ()
-	{
-		m_Button = GetComponent<Button>();
-		if (m_Button) m_Button.onClick.AddListener(ShowAdPlacement);
+	// public string zoneId;
+	// Button m_Button;
+	// void Start ()
+	// {
+	// 	m_Button = GetComponent<Button>();
+	// 	if (m_Button) m_Button.onClick.AddListener(ShowAdPlacement);
 
-	}
-	void Update ()
-	{
-		if (m_Button)
-		{
-			if (string.IsNullOrEmpty(zoneId)) zoneId = null;
-			m_Button.interactable = Advertisement.IsReady(zoneId);
-		}
-	}
-	void ShowAdPlacement ()
-	{
-		if (string.IsNullOrEmpty(zoneId)) zoneId = null;
-		var options = new ShowOptions();
-		options.resultCallback = HandleShowResult;
-		Advertisement.Show(zoneId, options);
-	}
-	void HandleShowResult (ShowResult result)
-	{
-		switch(result)
-		{
-		case ShowResult.Finished:
-			Debug.Log ("Video completed. Offer a reward to the player.");
-			break;
-		case ShowResult.Skipped:
-			Debug.LogWarning ("Video was skipped.");
-			break;
-		case ShowResult.Failed:
-			Debug.LogError ("Video failed to show.");
-			break;
-		}
-	}
+	// }
+	// void Update ()
+	// {
+	// 	if (m_Button)
+	// 	{
+	// 		if (string.IsNullOrEmpty(zoneId)) zoneId = null;
+	// 		m_Button.interactable = Advertisement.IsReady(zoneId);
+	// 	}
+	// }
+	// void ShowAdPlacement ()
+	// {
+	// 	if (string.IsNullOrEmpty(zoneId)) zoneId = null;
+	// 	var options = new ShowOptions();
+	// 	options.resultCallback = HandleShowResult;
+	// 	Advertisement.Show(zoneId, options);
+	// }
+	// void HandleShowResult (ShowResult result)
+	// {
+	// 	switch(result)
+	// 	{
+	// 	case ShowResult.Finished:
+	// 		Debug.Log ("Video completed. Offer a reward to the player.");
+	// 		break;
+	// 	case ShowResult.Skipped:
+	// 		Debug.LogWarning ("Video was skipped.");
+	// 		break;
+	// 	case ShowResult.Failed:
+	// 		Debug.LogError ("Video failed to show.");
+	// 		break;
+	// 	}
+	// }
 
 
 }
